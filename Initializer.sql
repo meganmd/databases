b07a9abe2c776c865123ba7b030d549e67da759e
mmdaly <mmdaly@csbsju.edu>
2016-11-05 14:24:06 -0500

    updated constraints on create tables

--- Initializer.sql
+++ Initializer.sql
@@ -5,9 +5,9 @@ DROP TABLE account CASCADE CONSTRAINTS;
 CREATE TABLE account (
   username varchar(15) not null,
   password varchar(15) not null,
-  isAdmin char not null check(isAdmin in (0,1)),
-  isSeller char not null check(isSeller in (0,1)),
-  primary key(username)
+  is_admin char not null check(is_admin in (0,1)),
+  is_seller char not null check(is_seller in (0,1)),
+  CONSTRAINT pk_account primary key(username)
 );
 
 --Customer
@@ -16,47 +16,57 @@ CREATE TABLE customer (
   username varchar(15) not null,
   phone varchar(10),
   email varchar(254),
-  fName varchar(25),
-  lName varchar(25),
-  primary key(username),
-  CONSTRAINT FK_username foreign key(username) references account(username)
+  fname varchar(25),
+  lname varchar(25),
+  CONSTRAINT pk_customer primary key(username),
+  CONSTRAINT fk_customer_to_account 
+    foreign key(username) references account(username)
+    on delete cascade
 );
 
 --Auction
 DROP TABLE auction CASCADE CONSTRAINTS;
 CREATE TABLE auction (
-  itemId varchar(15) not null,
+  item_id varchar(15) not null,
   seller varchar(15) not null,
-  startTime date not null,
-  endTime date not null,
-  startingPrice number(*, 2) not null,
-  itemName varchar(25) not null,
-  itemDescription varchar(300),
-  itemCategory varchar(25), --should maybe have a table for these? could ask Imad
-  primary key(itemId),
-  CONSTRAINT FK_sellerUsername foreign key(seller) references account(username) -- where account isSeller ??
+  start_time date not null,
+  end_time date not null,
+  starting_price number(*, 2) not null,
+  item_name varchar(25) not null,
+  item_description varchar(300),
+  item_category varchar(25), --should maybe have a table for these? could ask Imad
+  CONSTRAINT pk_auction primary key(item_id),
+  CONSTRAINT fk_auction_to_account 
+    foreign key(seller) references account(username) -- where account isSeller ??
+    on delete cascade
 );
 
 --Bid
 DROP TABLE bid CASCADE CONSTRAINTS;
 CREATE TABLE bid (
-  auctionId varchar(15) not null,
+  auction_id varchar(15) not null,
   customer varchar(15) not null,
-  time timestamp not null,
-  maximumBidLimit number(*,2) not null,
-  primary key(auctionId),
-  CONSTRAINT FK_customerUsername foreign key(customer) references account(username),
-  CONSTRAINT FK_bidToAuctionId foreign key(auctionId) references auction(itemId)
+  bid_time timestamp not null,
+  maximum_bid_limit number(*,2) not null,
+  CONSTRAINT pk_bid primary key(auction_id, customer, bid_time),
+  CONSTRAINT fk_bid_to_account 
+    foreign key(customer) references account(username)
+    on delete cascade,
+  CONSTRAINT fk_bid_to_auction 
+    foreign key(auction_id) references auction(item_id)
+    on delete cascade
 );
 
 --Feedback
 DROP TABLE feedback CASCADE CONSTRAINTS;
 CREATE TABLE feedback (
   auction varchar(15) not null,
-  overallRating char(1) not null check(overallRating in(1, 2, 3, 4, 5)),
-  qualityRating char(1) not null check(qualityRating in(1, 2, 3, 4, 5)),
-  deliveryRating char(1) not null check(deliveryRating in (1, 2, 3, 4, 5)),
+  overall_rating char(1) not null check(overall_rating in(1, 2, 3, 4, 5)),
+  quality_rating char(1) not null check(quality_rating in(1, 2, 3, 4, 5)),
+  delivery_rating char(1) not null check(delivery_rating in (1, 2, 3, 4, 5)),
   comments varchar(300) not null,
-  primary key(auction),
-  CONSTRAINT FK_feedbackToAuctionId foreign key(auction) references auction(itemId)
+  CONSTRAINT pk_feedback primary key(auction),
+  CONSTRAINT fk_feedback_to_auction 
+    foreign key(auction) references auction(item_id)
+    on delete cascade
 );
