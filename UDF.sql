c52ed89aa06c9f5097e5baeedb3cac1f99bdeb12
mmdaly <mmdaly@csbsju.edu>
2016-11-10 15:16:58 -0600

    Added function get_status(item_id string)

--- UDF.sql
+++ UDF.sql
@@ -1,5 +1,26 @@
---User defined functions
---status of auction
+--functions procedures and views
 
+--get status
+create or replace function get_status(item string) return string as
+status varchar2(15);
+begin
+  select case
+    when sysdate >= ( select start_time from auction where item_id = item)
+    and sysdate <= (select end_time from auction where item_id = item)
+    then 'ON AUCTION'
+    when sysdate > (select end_time from auction where item_id = item)
+    and exists (select * from bid where auction_id = item)
+    then 'SOLD'
+    when sysdate > (select end_time from auction where item_id = item)
+    and not exists (select * from bid where auction_id = item)
+    then 'NOT ON AUCTION'
+    else 'ERROR'
+  end into status
+  from dual;
+  
+  return status;
+end;
 
---current bid of auction 
+-- test:
+select get_status(3) from dual;
+
