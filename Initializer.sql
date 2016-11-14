d4060fe84de1e0f57d4fa9a55df31af57c4ee21e
mmdaly <mmdaly@csbsju.edu>
2016-11-14 01:04:40 -0600

    Noted constraints we may wish to add probably with triggers

--- Initializer.sql
+++ Initializer.sql
@@ -7,8 +7,10 @@ CREATE TABLE account (
   username varchar(15) not null,
   password varchar(25) not null,
   is_admin char default 0 not null check(is_admin in (0,1)),
-  is_seller char default 0 not null check(is_seller in (0,1)),  --do we actually need this? sellers have auctions non sellers dont
+  is_seller char default 0 not null check(is_seller in (0,1)),  
   CONSTRAINT pk_account primary key(username)
+  -- I don't believe we actually need an is_seller attribute because 
+  -- an account becomes a seller when they list an item for auction
 );
 
 --Customer
@@ -23,6 +25,10 @@ CREATE TABLE customer (
   CONSTRAINT fk_customer_to_account 
     foreign key(username) references account(username)
     on delete cascade
+  -- more constraints:
+  -- admins cannot be customers
+  -- could have email format constraint
+  -- also could have constraint that phone be numbers
 );
 
 --Auction
@@ -40,6 +46,12 @@ CREATE TABLE auction (
   CONSTRAINT fk_auction_to_account 
     foreign key(seller) references account(username) -- where account isSeller ??
     on delete cascade
+  -- more constraints:
+  -- admins cannot sell stuff
+  -- it would be nice if we required an entry in customer so we had an email 
+  -- address and such but it isn't in the requirements
+  -- starting price cannot be negative
+  -- end time must be after start time / current time
 );
 
 --Bid
@@ -56,6 +68,12 @@ CREATE TABLE bid (
   CONSTRAINT fk_bid_to_auction 
     foreign key(auction_id) references auction(item_id)
     on delete cascade
+  -- more constraints:
+  -- bid must be made after auction start and before auction end
+  -- seller cannot bid on own auction
+  -- maximum_bid_limit must be greater than current_bid and/or starting_price
+  -- customer must have entered their customer info ?
+  -- can we please just require for anyone not an admin
 );
 
 --Feedback
@@ -70,4 +88,6 @@ CREATE TABLE feedback (
   CONSTRAINT fk_feedback_to_auction 
     foreign key(auction) references auction(item_id)
     on delete cascade
+  -- feedback can only be entered for auctions whose status is sold (AKA past
+  -- auction date and have bids)
 );
