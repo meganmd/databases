f9566fdef8c3e96a938b038a4aac7a826f8b8b77
mmdaly <mmdaly@csbsju.edu>
2016-11-15 16:06:58 -0600

    fixed insert in proc_add_new_user so values are correct

--- proc_add_new_user.sql
+++ proc_add_new_user.sql
@@ -4,7 +4,7 @@ create or replace Procedure PROC_ADD_NEW_USER(username1 varchar,password1 varcha
         INSERT INTO ACCOUNT (USERNAME, PASSWORD, IS_ADMIN, IS_SELLER)
         VALUES (username1, password1, isadmin, isseller);
         
-        INSERT INTO CUSTOMER (USERNAME,PHONE, EMAIL,FNAME, LNAME) 
-        VALUES (username1, password1, phone, email, fname, lname);
+        INSERT INTO CUSTOMER (USERNAME,PHONE, EMAIL, FNAME, LNAME) 
+        VALUES (username1, phone, email, fname, lname);
       
     END;
