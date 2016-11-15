c63577c75fa62953a9cbf32dca4ae7d1659b9968
EKSTENSRUD <ekstensrud@nx.ad.csbsju.edu>
2016-11-15 11:26:03 -0600

    Become seller with comments

--- BecomeSeller.sql
+++ BecomeSeller.sql
@@ -10,3 +10,7 @@ INSERT INTO AUCTION VALUES ('7', 'harrypotter', '01-SEP-2016','01-SEP-2017',
   30.00, 'Potion', 'Pollyjuice', 'Misc');
 SELECT * FROM ACCOUNT
 WHERE username = 'harrypotter';
+
+
+--This trigger changes the account information of a user when they post a new item for auction. The user's is_seller status is
+--then changed to one, indicating that they are a seller. 
