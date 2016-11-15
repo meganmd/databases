c84e13d3221a41b38c6cb6d89535effe38fe9a39
mmdaly <mmdaly@csbsju.edu>
2016-11-15 14:25:53 -0600

    fixed overall commision report to reference average rating instead of individual ratings

--- overallcommisionreport.sql
+++ overallcommisionreport.sql
@@ -1,14 +1,14 @@
 --View Overalll Commission Report
 CREATE OR REPLACE VIEW OVERALL_COMMISION_REPORT_View AS
-SELECT C.USERNAME,
-       C.FNAME,
-       C.LNAME,
-       C.EMAIL,
-       F.OVERALL_RATING,
-       B.MAXIMUM_BID_LIMIT *0.05 AS "COMMISSION"
+SELECT account.USERNAME as username,
+       FNAME,
+       LNAME,
+       EMAIL,
+       seller_average_rating(account.username) as rating,
+       seller_number_of_auctions(account.username) as commissions
        
-FROM BID B
-  JOIN CUSTOMER C ON C.USERNAME = B.CUSTOMER 
-  JOIN FEEDBACK F ON B.AUCTION_ID = F.AUCTION
-
-ORDER BY C.USERNAME;
+FROM account left join customer on account.username = customer.username
+where exists (
+  Select * from auction 
+  where seller = account.username
+);
