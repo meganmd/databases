cbbf6c3d1f5f2636f3aef6608b6bd749fba1ab78
IAPARKER <iaparker@nx.ad.csbsju.edu>
2016-11-15 11:58:57 -0600

    adding procedure files for view user and add user

---
+++ proc_view_users.sql
@@ -0,0 +1,6 @@
+create or replace Procedure PROC_VIEW_USERS(username1 varchar,password1 varchar, isadmin char, isseller char, phone char, email varchar, fname varchar, lname varchar)
+ AS
+    Begin 
+        
+      
+    END;
