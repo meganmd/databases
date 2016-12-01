3602f466a0e658081f45a14320fb11b9a25b5694
mmdaly <mmdaly@csbsju.edu>
2016-11-30 20:51:58 -0600

    deleted extra files

--- proc_add_new_user.sql
+++ proc_add_new_user.sql
@@ -1,10 +1,9 @@
-create or replace Procedure PROC_ADD_NEW_USER(username1 varchar,password1 varchar, isadmin char, isseller char, phone char, email varchar, fname varchar, lname varchar)
+create or replace Procedure PROC_ADD_NEW_USER(
+    username1 varchar,password1 varchar, isadmin1 char, 
+    phone1 char, email1 varchar, 
+    fname1 varchar, lname1 varchar)
  AS
     Begin 
-        INSERT INTO ACCOUNT (USERNAME, PASSWORD, IS_ADMIN, IS_SELLER)
-        VALUES (username1, password1, isadmin, isseller);
-        
-        INSERT INTO CUSTOMER (USERNAME,PHONE, EMAIL, FNAME, LNAME) 
-        VALUES (username1, phone, email, fname, lname);
-      
+        INSERT INTO ACCOUNT (USERNAME, PASSWORD, PHONE, EMAIL, FNAME, LNAME, IS_ADMIN)
+        VALUES (username1, password1, phone1, email1, fname, lname, isadmin1);      
     END;
--- proc_view_users.sql
+++
@@ -1,6 +0,0 @@
-create or replace Procedure PROC_VIEW_USERS(username1 varchar,password1 varchar, isadmin char, isseller char, phone char, email varchar, fname varchar, lname varchar)
- AS
-    Begin 
-        
-      
-    END;
