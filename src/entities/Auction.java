6716261fdb8ce5f2973338235e03d587d14b697f
IAPARKER <iaparker@nx.ad.csbsju.edu>
2016-12-05 20:10:01 -0600

    Updated Auction.java and Bid.java

--- src/entities/Auction.java
+++ src/entities/Auction.java
@@ -1,79 +1,137 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Auction {
-	
-private static int itemid;
+//Create J Unit test file for Auction and Bid
+private int itemid;
 
-private static String seller;
-
-
-private static String startTime;
-
-private static String endTime;
-
-private static String itemName;
-
-private static String itemDescription;
-
-private static String itemCategory;
+private String seller;
 
 
+private String startTime;
 
+private String endTime;
+
+private String itemName;
+
+private String itemDescription;
+
+private String itemCategory;
+
+
+//Variable of type database connection
+private Connection myConnection;
+
+//Variable of type prepared statement
+private CallableStatement callStmt;  
+
+
+//Variables of type ResultSet which will contain query output
+private ResultSet result;
 
 //Setter and Getters
-public static int getItemid() {
+public int getItemid() {
 	return itemid;
 }
 
-public static void setItemid(int itemid) {
+public void setItemid(int itemid) {
 	Auction.itemid = itemid;
 }
 
-public static String getSeller() {
+public String getSeller() {
 	return seller;
 }
 
-public static void setSeller(String seller) {
+public void setSeller(String seller) {
 	Auction.seller = seller;
 }
 
-public static String getStartTime() {
+public String getStartTime() {
 	return startTime;
 }
 
-public static void setStartTime(String startTime) {
+public void setStartTime(String startTime) {
 	Auction.startTime = startTime;
 }
 
-public static String getEndTime() {
-	return endTime;
+public String getEndTime() {
+	return endTime;uteQuery(sql);
+	return results;
+	} catch (SQLException E) {
+	      E.printStackTrace();
 }
 
-public static void setEndTime(String endTime) {
+public void setEndTime(String endTime) {
 	Auction.endTime = endTime;
 }
 
-public static String getItemName() {
+public String getItemName() {
 	return itemName;
 }
 
-public static void setItemName(String itemName) {
+public void setItemName(String itemName) {
 	Auction.itemName = itemName;
 }
 
-public static String getItemDescription() {
+public String getItemDescription() {
 	return itemDescription;
 }
 
-public static void setItemDescription(String itemDescription) {
+public void setItemDescription(String itemDescription) {
 	Auction.itemDescription = itemDescription;
 }
 
-public static String getItemCategory() {
+public String getItemCategory() {
 	return itemCategory;
 }
 
-public static void setItemCategory(String itemCategory) {
+public void setItemCategory(String itemCategory) {
 	Auction.itemCategory = itemCategory;
+}
+
+
+public Connection openDBConnection() {
+    try {
+      // Load driver and link to driver manager
+      Class.forName("oracle.jdbc.OracleDriver");
+      // Create a connection to the specified database
+      Connection myConnection = DriverManager.getConnection("jdbc:oracle:thin:@//cscioraclesrv.ad.csbsju.edu:1521/" +
+                                                            "csci.cscioraclesrv.ad.csbsju.edu","team4", "Pds3D");
+      return myConnection;
+    } catch (Exception E) {
+      E.printStackTrace();
+    }
+    return null;
+  }
+
+public ResultSet executeStatement(String sql){
+	try{
+	Connection con = openDBConnection();
+	Statement stmt = con.createStatement();
+	ResultSet results = stmt.executeQuery(sql);
+	return results;
+	} catch (SQLException E) {
+	      E.printStackTrace();
+	    }
+	return null;
+}
+  
+public void createAuction(int itemid, String seller, String startTime, String endTime, String itemName, String itemDescription, String itemCategory){
+	ResultSet results = executeStatement(sql);
+}
+	//getBidderList -using items_bid_on.sql
+public void getBidderList(int itemid, String seller, String startTime, String endTime, String itemName, String itemDescription, String itemCategory){
+	String sql = 
+	ResultSet results = executeStatement(sql);
+	
+}
+	//getWinner
+	
+	//resultset getAuction
+	
 }
 
 }
