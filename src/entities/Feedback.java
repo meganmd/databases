34eafd8876622ca886bf422ac8fb920ff2adcf1a
IAPARKER <iaparker@nx.ad.csbsju.edu>
2016-12-11 14:31:05 -0600

    Updated Feedback.java

--- src/entities/Feedback.java
+++ src/entities/Feedback.java
@@ -15,11 +15,49 @@ public class Feedback implements Serializable{
 	private String qualityRating;
 	private String deliveryRating;
 	private String comments;
+	
 
 	public void createFeedback(String auction, String overallRating, String qualityRating, String deliveryRating, String comments){
-	
+		try{
+			ResultSet results;
+			Connection con = openDBConnection();
+			PreparedStatement stmt;
+			String query = "INSERT INTO auction values(?, ?, ?,?, ?, ?)";
+			stmt=con.prepareStatement(query);
+			stmt.setString(1, auction);
+			stmt.setString(2, overallRating);
+			stmt.setBoolean(3, qualityRating);
+			stmt.setString(4, deliveryRating);
+			stmt.setString(5, comments);
+			results = stmt.executeQuery();
+			stmt.close();
+			con.close();
+		}
+		catch(SQLException e){
+				System.out.println("SQL issue: " + e);
+			}	
+		
 	}
+	
 	public ResultSet getFeedback(String username) throws IllegalStateException{
+		try{
+			ResultSet results;
+			Connection con = openDBConnection();
+			PreparedStatement stmt;
+			String query = "SELECT * FROM feedback WHERE auction= ?";
+			stmt=con.prepareStatement(query);
+			stmt.setString(1, auction);
+			results = stmt.executeQuery();
+			stmt.close();
+			con.close();
+			
+			return results;
+		}
+		
+		catch(SQLException e){
+				System.out.println("SQL issue: " + e);
+			}
+		
 		return null;
 	}
 }
