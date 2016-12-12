761e3b937b1f657f845b71e22a042b5c144ba177
mmdaly <mmdaly@csbsju.edu>
2016-12-12 08:00:02 -0600

    made login work and redirect to either adminhome or userhome

--- src/entitiesTest/AccountTest.java
+++ src/entitiesTest/AccountTest.java
@@ -2,6 +2,7 @@ package entitiesTest;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,16 +10,30 @@ import entities.Account;
 
 public class AccountTest {
 	Account acct;
+	Account admin;
+	Account user;
 	
 	@Before
 	public void init() throws Exception {
 		acct = new Account();
 		acct.setUsername("ekstensrud");
 		acct.setLname("Stensrud");
+		
+		admin = new Account();
+		admin.setUsername("admin");
+		admin.setPassword("super");
+		
+		user = new Account();
+		user.setUsername("user1");
+		user.setPassword("password1");
 	}
 	@Test
-	public void testLogin() throws Exception {
+	public void testLogin() {
+		Assert.assertTrue("admin can login", admin.login());
+		Assert.assertTrue("user can log in", user.login());
 		
+		Assert.assertTrue("admin shows as admin", admin.isAdmin());
+		Assert.assertTrue("user is not admin", !user.isAdmin());
 	}
 
 }
